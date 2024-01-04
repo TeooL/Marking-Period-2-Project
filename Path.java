462c841996e38598f1434268d6d1689bcf07c1c7
TeooL <144486916+TeooL@users.noreply.github.com>
2024-01-04 15:03:14 -0500

    Delete Path.java

--- Path.java
+++
@@ -1,9 +0,0 @@
-public class Path {
-    private String name; // Displays the name of the path (Warrior, Ranger or Rogue);
-    private int pathID; // 1 = Warrior, 2 = Ranger, 3 = Rogue;
-
-    public Path(String name, int ID){
-        this.name = name;
-        pathID = ID;
-    }
-}
